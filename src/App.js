d9430c76f23801f4affdc1ddd4878f08dc68d137
kathrynzaro <zarokathryn@gmail.com>
2022-06-30 15:24:35 -0700

    importing react router, adding routes to admin

--- src/App.js
+++ src/App.js
@@ -1,63 +1,25 @@
-import './App.css';
-import { useState } from 'react';
-import OpenSign from './OpenSign';
-import Festival from './Festival';
-import backgroundImg from './halloween.png';
-import CustomButton from './CustomButton';
+import { BrowserRouter as Router, Route, Routes, Link, Redirect } from 'react-router-dom';
+import Zoo from './Zoo';
+import Admin from './Admin';
 
-function App() {
 
-  const [festivalOpen, setFestivalOpen] = useState(true);
-  const [monsters, setMonsters] = useState(['vampire', 'witch', 'frankenstein', 'ghost']);
-  const [batSize, setBatSize] = useState(12);
-  const [catSize, setCatSize] = useState(11);
-  
-  function handleVampireClick() {
-    monsters.push('vampire');
-    setMonsters(monsters.slice());
-  }
-  function handleWitchClick() {
-    monsters.push('witch');
-    setMonsters(monsters.slice());
-  }
-  function handleFrankensteinClick() {
-    monsters.push('frankenstein');
-    setMonsters(monsters.slice());
-  }
-  function handleGhostClick() {
-    monsters.push('ghost');
-    setMonsters(monsters.slice());
-  }
+
+
+export default function App() {
 
   return (
-    <div className="App" style={{ backgroundImage: `url(${backgroundImg})` }}>
-      <OpenSign festivalOpen={festivalOpen} />
-      <CustomButton onClick={() => setFestivalOpen(!festivalOpen)}>wanna go?</CustomButton>
-      <div className='fight'>
-        <div className='competitor'>
-          <img src='bat.png' width={12 * batSize} />
-          <div className='fight-buttons'>
-            <CustomButton onClick={() => setBatSize(batSize + 1)}>grow bat</CustomButton>
-            <CustomButton onClick={() => setCatSize(catSize - 1)}>shrink cat</CustomButton>
-          </div>
-        </div>
-        <div className='competitor'>
-          <img src='cat.png' width={11 * catSize} />
-          <div className='fight-buttons'>
-            <CustomButton onClick={() => setCatSize(catSize + 1)}>grow cat</CustomButton>
-            <CustomButton onClick={() => setBatSize(batSize - 1)}>shrink bat</CustomButton>
-          </div>
-        </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/admin">Admin</Link>
+          </li>
+        </ul>
       </div>
-      <Festival monsters={monsters} />
-      <div className='buttons'>
-        <CustomButton onClick={handleVampireClick}>add vampire</CustomButton>
-        <CustomButton onClick={handleWitchClick}>add witch</CustomButton>
-        <CustomButton onClick={handleFrankensteinClick}>add frankenstein</CustomButton>
-        <CustomButton onClick={handleGhostClick}>add ghost</CustomButton>
-      </div>
-    </div>
+      <Routes>
+        <Route path="/" element={<Zoo />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
